d5e04755a5231596168a5c1e237b07c4915ba5be
boohyunsik <bhs9194@nate.com>
2021-07-11 22:05:37 +0900

    impl: google logout

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 
 function App() {
   // ........
-  // asdasjojdjasldjjaslkdjasdkljaslkdas
-  // sadasdasdasdaseaqsedasda
   return (
     <div className="App">
       <header className="App-header">
